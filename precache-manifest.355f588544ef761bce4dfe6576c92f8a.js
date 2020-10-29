865f03724b526f4c18d6ac4734dd7701bee9a880
Ramananaidu418 <mvr418@gmail.com>
2020-10-29 14:11:44 +0530

    Updates

---
+++ precache-manifest.355f588544ef761bce4dfe6576c92f8a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c0cb94f5f91c6d6931e3c2051b5d57",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "85c6ebbf6adfff05a310",
+    "url": "/gallery/static/css/main.a25b6792.chunk.css"
+  },
+  {
+    "revision": "d4e38a59a673bda1676e",
+    "url": "/gallery/static/js/2.f2d9956c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gallery/static/js/2.f2d9956c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c6ebbf6adfff05a310",
+    "url": "/gallery/static/js/main.b5594fc2.chunk.js"
+  },
+  {
+    "revision": "c8a0f149900244e3407e",
+    "url": "/gallery/static/js/runtime-main.79ee6c40.js"
+  },
+  {
+    "revision": "0e0fd452dc7a7d35c32269c388665363",
+    "url": "/gallery/static/media/hero.0e0fd452.jpg"
+  },
+  {
+    "revision": "1cad2dde38836a5bb7be6beabf27215c",
+    "url": "/gallery/static/media/img-02.1cad2dde.jpg"
+  },
+  {
+    "revision": "4b55adc478deec11d6ab5b8fcbd625f2",
+    "url": "/gallery/static/media/img-03.4b55adc4.jpg"
+  },
+  {
+    "revision": "8b181539ac4e3828db06c7ced6b59f94",
+    "url": "/gallery/static/media/img-04.8b181539.jpg"
+  },
+  {
+    "revision": "c03103b1fe51caa6a401a41620522675",
+    "url": "/gallery/static/media/img-05.c03103b1.jpg"
+  },
+  {
+    "revision": "df4ec360ffd0fe509e6a2fe5a5c4fd0d",
+    "url": "/gallery/static/media/img-07.df4ec360.jpg"
+  },
+  {
+    "revision": "4e7084d391d795c1963cb7b2ea4ad8fc",
+    "url": "/gallery/static/media/img-08.4e7084d3.jpg"
+  },
+  {
+    "revision": "971238e4b7640d650f7ee22119f861a2",
+    "url": "/gallery/static/media/img-09.971238e4.jpg"
+  },
+  {
+    "revision": "12776cb65c2c279ca067aa21b84f24f4",
+    "url": "/gallery/static/media/img-10.12776cb6.jpg"
+  },
+  {
+    "revision": "d86db5f5f9ff915444ff55529088f3a3",
+    "url": "/gallery/static/media/img-11.d86db5f5.jpg"
+  },
+  {
+    "revision": "2b804a662819c29386eb91dafa4a40ca",
+    "url": "/gallery/static/media/img-12.2b804a66.jpg"
+  },
+  {
+    "revision": "9dd66784bc9123534dd39370ea407e2b",
+    "url": "/gallery/static/media/img1.9dd66784.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gallery/static/media/logo.5d5d9eef.svg"
+  }
+]);
